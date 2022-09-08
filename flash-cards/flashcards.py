42aee8492dbecf2d1981132c2eb12c92e3ad0a6d
Alex Rocha <rocha.amc@gmail.com>
2022-09-07 21:20:52 -0700

    Add comments

--- flash-cards/flashcards.py
+++ flash-cards/flashcards.py
@@ -1,39 +1,45 @@
 import json
 import random
 
-deck = 'me-capitals.json'
+deck = "me-capitals.json"
 
+# Open the chosen file and return as a list so it can be randomized with random.shuffle
 def load_deck(file):
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         return list(json.load(f)["cards"])
+
 
 data = load_deck(deck)
 
+# Randomize the order of questions and answers
 def randomize_data(data):
     random.shuffle(data)
 
+# Check if inputted answer is correct
 def ask_question(question, answer):
-    guess = input(f'{question} > ')
-
+    guess = input(f"{question} > ")
     if guess.lower() == answer.lower():
         return True
     else:
         return False
 
+# End game score
 def end_game(score, total):
-    print(f'Thanks for playing! You scored: {score} out of {total} correct!')
+    print(f"Thanks for playing! You scored: {score} out of {total} correct!")
     if score == total:
-        print('Amazing...')
-    elif score > total/2:
-        print('Good work...')
+        print("Amazing...")
+    elif score > total / 2:
+        print("Good work...")
     else:
-        print('You need practice...')
+        print("You need practice...")
 
+# Check if player wants to play again and load another game if yes
 def play_again():
-    result = input('Play Again? (Y/N) > ')
-    if result.lower() == 'y':
+    result = input("Play Again? (Y/N) > ")
+    if result.lower() == "y":
         start_game(data)
 
+# Main game loop
 def start_game(data):
     # initialize total as the length of the cards array
     total = len(data)
@@ -48,22 +54,15 @@ def start_game(data):
             # increment score up one
             score += 1
             # interpolate score and total into the response
-            print(f'Correct! Current score: {score}/{total}')
+            print(f"Correct! Current score: {score}/{total}")
         else:
             print(f'Incorrect! The correct answer was {i["a"]}')
-            print(f'Current score: {score}/{total}')
+            print(f"Current score: {score}/{total}")
 
-    end_game(score,total)
+    end_game(score, total)
     play_again()
+
 
 start_game(data)
 
-print('Game Over')
-
-
-# Stretch Challenges:
-
-# Keep playing until the player gets all the questions right at least once.
-# Keep playing until the player answers correctly 10 in a row.
-# Create various data files that are different sets of questions and let people pick which one they want to do.
-# Let people enter their own cards and save those as libraries of questions and answers.
+print("Game Over")
